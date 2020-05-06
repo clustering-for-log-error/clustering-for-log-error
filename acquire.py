72247c833377dd43f104884ebad816fc97cd6229
Chase Thompson <chase.w.thompson210@gmail.com>
2020-05-06 15:34:40 -0500

    updated acquire file

--- acquire.py
+++ acquire.py
@@ -13,28 +13,43 @@ def get_zillow_data():
    And where bed and bath count are greater than 0
    ''' 
    query = '''
-   SELECT properties_2017.parcelid, bathroomcnt, bedroomcnt, buildingqualitytypeid, calculatedfinishedsquarefeet,  
-finishedsquarefeet12, fips, fullbathcnt, latitude, longitude, lotsizesquarefeet, propertycountylandusecode,     
-regionidcity, regionidcounty, regionidzip, roomcnt, unitcnt, yearbuilt, 
-structuretaxvaluedollarcnt,taxvaluedollarcnt, assessmentyear, 
-landtaxvaluedollarcnt, taxamount,                      
-predictions_2017.logerror,                       
-predictions_2017.transactiondate,                
-heatingorsystemdesc,            
-propertylandusedesc
-FROM properties_2017
-JOIN predictions_2017 USING(parcelid)
-LEFT JOIN airconditioningtype USING(airconditioningtypeid)
-LEFT JOIN architecturalstyletype USING(architecturalstyletypeid)
-LEFT JOIN buildingclasstype USING(buildingclasstypeid)
-LEFT JOIN heatingorsystemtype USING(heatingorsystemtypeid)
-LEFT JOIN propertylandusetype USING(propertylandusetypeid)
-LEFT JOIN storytype USING(storytypeid)
-LEFT JOIN typeconstructiontype USING(typeconstructiontypeid)
-WHERE latitude IS NOT NULL
-AND longitude IS NOT NULL
-AND bathroomcnt > 0
-AND bedroomcnt > 0
+   SELECT
+      properties_2017.parcelid,
+      bathroomcnt,
+      bedroomcnt,
+      buildingqualitytypeid,
+      calculatedfinishedsquarefeet,
+      fips,
+      latitude,
+      longitude,
+      lotsizesquarefeet,
+      propertycountylandusecode,   
+      regionidcity,
+      regionidcounty,
+      regionidzip,
+      roomcnt,
+      unitcnt,
+      yearbuilt, 
+      structuretaxvaluedollarcnt,
+      taxvaluedollarcnt, 
+      landtaxvaluedollarcnt,
+      taxamount,                      
+      predictions_2017.logerror,                       
+      predictions_2017.transactiondate
+   FROM properties_2017
+   JOIN predictions_2017 USING(parcelid)
+   LEFT JOIN airconditioningtype USING(airconditioningtypeid)
+   LEFT JOIN architecturalstyletype USING(architecturalstyletypeid)
+   LEFT JOIN buildingclasstype USING(buildingclasstypeid)
+   LEFT JOIN heatingorsystemtype USING(heatingorsystemtypeid)
+   LEFT JOIN propertylandusetype USING(propertylandusetypeid)
+   LEFT JOIN storytype USING(storytypeid)
+   LEFT JOIN typeconstructiontype USING(typeconstructiontypeid)
+   WHERE
+      latitude IS NOT NULL
+      AND longitude IS NOT NULL
+   AND bathroomcnt > 0
+   AND bedroomcnt > 0
    '''
    df = pd.read_sql(query, get_db_url('zillow'))
    return df
