96ec814a491e9a039fe737942cc4502d9269b1bf
jadashipp <jada.l.shipp@gmail.com>
2020-05-07 10:26:59 -0500

    added ratio function

--- prepare.py
+++ prepare.py
@@ -12,7 +12,7 @@ def better_names(df):
     """
     # rename
     df = df.rename(columns={"calculatedfinishedsquarefeet":"square_footage", "structuretaxvaluedollarcnt":"house_value", "landtaxvaluedollarcnt":"land_value", "taxvaluedollarcnt":"full_value", "lotsizesquarefeet":"lot_size"})
-
+     
     # reorder
     df = df[["longitude", "latitude", "age", "month", "bedroomcnt", "bathroomcnt", "square_footage", "lot_size", "house_value", "land_value", "full_value", "tax_rate", "roomcnt", "Los_Angeles", "Orange", "Ventura", "logerror"]]
     return df
@@ -32,3 +32,7 @@ def transaction_month(df):
     df["month"] = pd.DatetimeIndex(df.transactiondate).month
     df.drop("transactiondate",axis=1, inplace=True)
     return df
+
+def bed_bath_ratio(df):
+    df['bed_bath_ratio'] = df.bedroomcnt/df.bathroomcnt
+return df
