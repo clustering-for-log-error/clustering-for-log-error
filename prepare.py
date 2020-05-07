260fe1a1741fe49c01863b772f2ba78dc54603f8
Chase Thompson <chase.w.thompson210@gmail.com>
2020-05-06 23:24:29 -0500

    created prepare file, updated others

---
+++ prepare.py
@@ -0,0 +1,34 @@
+import pandas as pd 
+import numpy as numpy
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+import warnings
+warnings.filterwarnings("ignore")
+
+def better_names(df):
+    """
+    Rename columns to easier to understand names. Then reorder columns based on relevancy to others.
+    """
+    # rename
+    df = df.rename(columns={"calculatedfinishedsquarefeet":"square_footage", "structuretaxvaluedollarcnt":"house_value", "landtaxvaluedollarcnt":"land_value", "taxvaluedollarcnt":"full_value", "lotsizesquarefeet":"lot_size"})
+
+    # reorder
+    df = df[["longitude", "latitude", "age", "month", "bedroomcnt", "bathroomcnt", "square_footage", "lot_size", "house_value", "land_value", "full_value", "tax_rate", "roomcnt", "Los_Angeles", "Orange", "Ventura", "logerror"]]
+    return df
+
+def tax_rate(df):
+    """
+    Create a new taxrate column and drop the taxamount column, as it no longer adds value.
+    """
+    df["tax_rate"] = df.taxamount / df.taxvaluedollarcnt
+    df.drop(columns=["taxamount"], inplace=True)
+    return df
+
+def transaction_month(df):
+    """
+    Replace the transactiondate with the month of the transaction.
+    """
+    df["month"] = pd.DatetimeIndex(df.transactiondate).month
+    df.drop("transactiondate",axis=1, inplace=True)
+    return df
--- wrangle.py
+++ wrangle.py
@@ -11,14 +11,8 @@ def handle_nulls(df):
     '''
     # Set nulls to be the median
 
-    median = df.finishedsquarefeet12.median()
-    df.finishedsquarefeet12 = df.finishedsquarefeet12.fillna(median)
-
     median = df.calculatedfinishedsquarefeet.median()
     df.calculatedfinishedsquarefeet = df.calculatedfinishedsquarefeet.fillna(median)
-
-    median = df.fullbathcnt.median()
-    df.fullbathcnt = df.fullbathcnt.fillna(median)
 
     median = df.lotsizesquarefeet.median()
     df.lotsizesquarefeet = df.lotsizesquarefeet.fillna(median)
@@ -29,39 +23,16 @@ def handle_nulls(df):
     median = df.taxamount.median()
     df.taxamount = df.taxamount.fillna(median)
 
-    median = df.taxamount.median()
-    df.taxamount = df.taxamount.fillna(median)
-
     median = df.structuretaxvaluedollarcnt.median()
     df.structuretaxvaluedollarcnt = df.structuretaxvaluedollarcnt.fillna(median)
-
-    median = df.structuretaxvaluedollarcnt.median()
-    df.structuretaxvaluedollarcnt = df.structuretaxvaluedollarcnt.fillna(median)
-
-    df.buildingqualitytypeid = df.buildingqualitytypeid.fillna(df.buildingqualitytypeid.median())
-
 
     # Use the mode for nulls
-    mode = df.regionidcity.mode()
-    df.regionidcity = df.regionidcity.fillna(mode)
-
-    mode = df.regionidzip.mode()
-    df.regionidzip = df.regionidzip.fillna(mode)
 
     mode = df.yearbuilt.mode()
     df.yearbuilt = df.yearbuilt.fillna(mode)
 
     mode = df.landtaxvaluedollarcnt.mode()
     df.landtaxvaluedollarcnt = df.landtaxvaluedollarcnt.fillna(mode)
-
-    # Imputing with most common observation
-    df.heatingorsystemdesc = df.heatingorsystemdesc.fillna("None")
-    df.unitcnt = df.unitcnt.fillna(1.0)
-    df.unitcnt = df.unitcnt.fillna(1.0)
-
-
-    # drop regionidneighborhood and transactiondate
-    df = df.drop(columns=['transactiondate'])
 
     # drop all remaining rows with null values
     df = df.dropna()
@@ -83,4 +54,5 @@ def prepare_zillow(df):
 
     # calculate age of home
     df['age'] = 2017 - df.yearbuilt
+    df.drop(columns=['yearbuilt'], inplace=True)
     return df
